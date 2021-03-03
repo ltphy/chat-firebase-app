aaa8cadc6ab8446c9b210d4ebcbc38476048a6c4
Phy Lieng <phylieng97@gmail.com>
2021-03-03 21:45:55 +0700

    feat<firbase_chatapp> settings to use emulator

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:chat_app_firebase/screens/bands_screen/bands_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-bool USE_FIRESTORE_EMULATOR = false;
+
+bool USE_FIRESTORE_EMULATOR = true;
 
 void main() async {
   //
   WidgetsFlutterBinding.ensureInitialized();
   // initialized firebase
   await Firebase.initializeApp();
-  if(USE_FIRESTORE_EMULATOR) {
-
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
   }
   runApp(MyApp());
 }
